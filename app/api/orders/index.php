aba370db833239efca3b69477a3ba4e205efcc20
skmishra119 <skmishra119@gmail.com>
2019-01-27 20:45:46 +0530

    SKM: updated on Jan 27,  2019 - II

--- app/api/orders/index.php
+++ app/api/orders/index.php
@@ -18,10 +18,10 @@ $app->post('/api/order/{lids}', function($request){
 	$patient_id = $request->getParam('patient_id');
 	$doctor_id = $request->getParam('doctor_id');
 	$collector_id = $request->getParam('collector_id');
-	$order_date = $request->getParam('order_date');
+	$order_date = DateTime::createFromFormat('d/m/Y', $request->getParam('order_date'))->format('Y-m-d');
 	$barcode = $request->getParam('barcode');
 	$status = $request->getParam('status');
-	$items = !empty($request->getParam('prod_ids')) ? explode(",",$request->getParam('prod_ids')) : [];
+	$items = !empty($request->getParam('prod_ids')) ? $request->getParam('prod_ids') : [];
 
 	$qry="insert into bl_orders (id, order_date, patient_id, doctor_id, collector_id, barcode, status) values (:newid, :order_dt, :patient_id, :doctor_id, :collector_id, :barcode,  :status)";
 	try{
@@ -72,7 +72,7 @@ $app->get('/api/order/{lids}', function($request){
 	$lu_ids = explode('::',$request->getAttribute('lids'));
 	$lab_id = trim($lu_ids[0]);
 	$pid = trim($lu_ids[1]);
-	$qry = "select id, barcode, patient_id, doctor_id, collector_id, barcode, status, date_format(updated,'%b %d, %Y %H:%i:%s') as updated FROM bl_orders where id='$pid' AND status='ACTIVE'";
+	$qry = "select id, barcode, date_format(order_date,'%d/%m/%Y') as order_date, patient_id, doctor_id, collector_id, barcode, status, date_format(updated,'%b %d, %Y %H:%i:%s') as updated FROM bl_orders where id='$pid' AND status='ACTIVE'";
 	
 	try{
 		$lab_db = new lab_db();
@@ -83,10 +83,10 @@ $app->get('/api/order/{lids}', function($request){
 		$stmt = $lab_db->query($qry);
 		$data['data'] = $stmt->fetchAll(PDO::FETCH_OBJ);
 		$lab_db = null;
-		/*if(sizeof($data['data'])>0){
+		if(sizeof($data['data'])>0){
 			$order_info = (array)current($data['data']);
-			$order_info['items'] = array();
-			$qry = "select id, date_format(updated,'%b %d, %Y %H:%i:%s') as updated FROM bl_order_products where order_id='$pid'"; // Need to join with item table to get item more info
+			$order_info['prod_ids'] = array();
+			$qry = "select id FROM bl_order_products where order_id='$pid'"; // Need to join with item table to get item more info
 
 			$lab_db = new lab_db();
 			$lab_db = $lab_db->connect($lab_id);
@@ -94,13 +94,13 @@ $app->get('/api/order/{lids}', function($request){
 				throw new PDOException("Internal server error in connecting databases", 1);
 			}
 			$stmt = $lab_db->query($qry);
-			$order_info['items'] = $stmt->fetchAll(PDO::FETCH_OBJ);
+			$order_info['prod_ids'] = $stmt->fetchAll(PDO::FETCH_OBJ);
 			$data['data'] = $order_info;
 			$data['message'] = array('type'=>'success', 'msg'=>'Success');
 		} else {
-			//$data['data'] = array(array('token'=>null));
+			$data['data'] = array(array('token'=>null));
 			$data['message'] = array('type'=>'Error', 'msg'=>'No data available!');	
-		}*/
+		}
 		$data['message'] = array('type'=>'success', 'msg'=>'Success');
 		echo json_encode(array_reverse($data));
 	} catch(PDOException $e){
@@ -113,7 +113,7 @@ $app->get('/api/orders/{lids}', function($request){
 	$lu_ids = explode('::',$request->getAttribute('lids'));
 	$lab_id = trim($lu_ids[0]);
 
-	$qry = "select o.id, concat(p.title,' ',p.first_name,' ',p.last_name) as patient, concat(d.title,' ',d.first_name,' ',d.last_name) as doctor, concat(c.title,' ',c.first_name,' ',c.last_name) as collector, o.barcode, o.status, date_format(o.updated,'%b %d, %Y %H:%i:%s') as updated FROM bl_orders o left join bl_patients p on o.patient_id=p.id left join bl_doctors d on o.doctor_id=d.id left join bl_collectors c on o.collector_id=c.id where o.status='ACTIVE'";
+	$qry = "select o.id, date_format(o.order_date,'%b %d, %Y') as order_date, concat(p.title,' ',p.first_name,' ',p.last_name) as patient, concat(d.title,' ',d.first_name,' ',d.last_name) as doctor, concat(c.title,' ',c.first_name,' ',c.last_name) as collector, o.barcode, o.status, date_format(o.updated,'%b %d, %Y %H:%i:%s') as updated FROM bl_orders o left join bl_patients p on o.patient_id=p.id left join bl_doctors d on o.doctor_id=d.id left join bl_collectors c on o.collector_id=c.id where o.status='ACTIVE' order by o.order_date desc";
 	try{
 		$lab_db = new lab_db();
 		$lab_db = $lab_db->connect($lab_id);
@@ -123,8 +123,7 @@ $app->get('/api/orders/{lids}', function($request){
 		$stmt = $lab_db->query($qry);
 		$data['data'] = $stmt->fetchAll(PDO::FETCH_OBJ);
 		$lab_db = null;
-		if(sizeof($data['data'])>0){
-
+		/*if(sizeof($data['data'])>0){
 			$orders_data = array();
 			foreach ($data['data'] as $key => $value) {
 				$orders_data[0] = (array)$value;
@@ -138,14 +137,13 @@ $app->get('/api/orders/{lids}', function($request){
 				}
 				$stmt = $lab_db->query($qry);
 				$orders_data[0]['items'] = $stmt->fetchAll(PDO::FETCH_OBJ);
-				
 			}
 			$data['data'] = $orders_data;
 			$data['message'] = array('type'=>'success', 'msg'=>'Success');
 		} else {
 			//$data['data'] = array(array('token'=>null));
 			$data['message'] = array('type'=>'Error', 'msg'=>'No data available!');	
-		}
+		}*/
 		echo json_encode(array_reverse($data));
 	} catch(PDOException $e){
 		echo '{"message" : {type": "Error", "msg": "'.$e->getMessage().'"}}';
@@ -154,7 +152,6 @@ $app->get('/api/orders/{lids}', function($request){
 
 // order update API 
 $app->put('/api/order/{lids}', function($request){
-
 	$lu_ids = explode('::',$request->getAttribute('lids'));
 	$lab_id = trim($lu_ids[0]);
 	$oid = trim($lu_ids[1]);
@@ -165,10 +162,10 @@ $app->put('/api/order/{lids}', function($request){
 	$patient_id = $request->getParam('patient_id');
 	$doctor_id = $request->getParam('doctor_id');
 	$collector_id = $request->getParam('collector_id');
-	$order_date = $request->getParam('order_date');
+	$order_date = DateTime::createFromFormat('d/m/Y', $request->getParam('order_date'))->format('Y-m-d');
 	$barcode = $request->getParam('barcode');
 	$status = $request->getParam('status');
-	$items = !empty($request->getParam('items')) ? $request->getParam('items') : [];
+	$items = !empty($request->getParam('prod_ids')) ? $request->getParam('prod_ids') : [];
 
 	$qry="UPDATE bl_orders SET patient_id = :patient_id, doctor_id = :doctor_id,  collector_id = :collector_id,  barcode = :barcode, order_date = :order_dt, status = :status WHERE id = :oid";
 
